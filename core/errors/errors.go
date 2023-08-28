325c226a2165973a32d96b93dd614026c28b5268
Naru Koshin <>
2023-08-29 02:10:00 +0300

    Added saving level 4

--- core/errors/errors.go
+++ core/errors/errors.go
@@ -5,4 +5,4 @@ import "errors"
 var ErrSalaryRequired 			= errors.New("Salary is required to calculate the expenses")
 var	ErrExpensesMoreThanSalary 	= errors.New("Nothing to save, expenses are more than salary")
 var ErrWithdrawnAmountTooLow 	= errors.New("Sorry, for now it seems that your salary is too small to make additional savings.")
-var ErrLevelTooHigh 			= errors.New("You can't set level higher than 3.")
+var ErrLevelTooHigh 			= errors.New("You can't set level higher than 4.")
